f45ea3f18c26cc56f71f245081b3811aa6d8ceac
J. Wilczek <85438277+julita-wilczek@users.noreply.github.com>
2022-03-01 20:26:08 +0100

    Set all CRUD methods

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,4 +1,4 @@
-import express from "express"  // need to install it with npm i express
+import express, { response } from "express"  // need to install it with npm i express
 import { fileURLToPath } from "url" // no need to install it, it's a core package
 import { dirname, join } from "path" // no need to install it, it's a core package
 import fs from "fs" // no need to install it, it's a core package.
@@ -10,7 +10,7 @@ import uniqid from "uniqid" // need to install it with npm i uniqid
 // Then I turn this url into path using fileURLToPath() - need to import it from url
 // Then I get the parent's folder path of the current file using dirname() - need to import it from path
 // Then I add (concatenate) the name of the file I want to link to, using  join() - need to import that also from path
-join(dirname(fileURLToPath(import.meta.url)), "authors.json")
+const authorsJSONPath = join(dirname(fileURLToPath(import.meta.url)), "authors.json")
 
 const authorsRouter = express.Router() // all the enpoints attached to this router will have http://localhost:3001/authors as PREFIX
 
@@ -18,111 +18,68 @@ authorsRouter.post("/", (request, response) => { // this is used for posting a n
 // In this CRUD method I will receive a body as a part of the request
 // To get it I need to write request.body
 // If the request.body is empty then it means I did not add server.use(express.json()) in server.js file!!!
-
-
+// With the request.body I create a new user, adding some server info with spread operator
+// I use uniqid, a 3rd party module to generate unique ids
+const newUser = {...request.body, createdAt: new Date(), _id: uniqid(), avatar: `https://ui-avatars.com/api/?name=${request.body.name}+${request.body.surname}`}
+//Then I get the current array from JSON file and change it into the regular array so I can add new object there
+const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath)) //
+authorsArray.push(newUser)
+// Then I write update the JSON file to reflect the change 
+fs.writeFileSync(authorsJSONPath, JSON.stringify(authorsArray)) // before we pass it we need to turn it back to json
+// Then send the response back // 201 means Created
+response.status(201).send(newUser) // this is what we send back to user in the response. Could also be just id {_id: newUser.id }
 })
 
-authorsRouter.get("/", (request, response) => {}) // this is used for gettingthe array of authors
-authorsRouter.get("/:authorId", (request, response) => {}) // this is used for getting a specific author object
-authorsRouter.put("/:authorId", (request, response) => {}) // this is used for updating a specific author object
-authorsRouter.delete("/:authorId", (request, response) => {}) // this is used for deleting a specific author object
+authorsRouter.get("/", (request, response) => {
+// First, we get the contents of authors.json file as a BUFFER object, sth that is machine readable only
+const fileContent = fs.readFileSync(authorsJSONPath)
+// Now we make is human readible with JSON.parse
+const authorsArray = JSON.parse(fileContent)
+// Now let's send it back to the user that requested it
+response.send(authorsArray)
+
+}) // this is used for getting the array of authors
+authorsRouter.get("/:authorId", (request, response) => { // this is used for getting a specific author object
+
+// First, get the array and make it human readible 
+const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath))
+// The authors id that the user requested is stored in request params
+const authorId = request.params.authorId
+// Now let's find the object the user requested in the array
+const requestedAuthor = authorsArray.find(author => author._id === authorId)
+// Now let's send it back to the user that requested it
+response.send(requestedAuthor)
+}) 
+
+authorsRouter.put("/:authorId", (request, response) => { // this is used for updating a specific author object
+// First, get the array and make it human readible 
+const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath))
+// The authors id that the user requested is stored in request params
+const authorId = request.params.authorId
+// Now let's find the indexOf the object so we can change the object later
+const index = authorsArray.findIndex(author => author._id === authorId)
+// So this is the author object before the changes
+const oldAuthor = authorsArray[index]
+// Here we ask to combine the current author object with any changes there are, plus added date of update for further reference
+const updatedAuthor = {...oldAuthor, ...request.body, updatedAt: new Date()}
+// Now we replace the old author object with the updated one, keeping the same index number
+authorsArray[index] = updatedAuthor
+// Let's save the changes in the authors.json file
+fs.writeFileSync(authorsJSONPath, JSON.stringify(authorsArray))
+// Let the user now what the updated authors file look like
+response.send(updatedAuthor)
+
+}) 
+authorsRouter.delete("/:authorId", (request, response) => {// this is used for deleting a specific author object
+//Again, get the file and it's content
+const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath))
+// Now create of it a new array that contains everything but the object requested
+const remainingAuthors = authorsArray.filter(author => author._id !== request.params.authorId)
+
+// Save the changes in the JSON file
+fs.writeFileSync(authorsJSONPath, JSON.stringify(remainingAuthors))
+// Send a proper response (204 means No Content but then you cannot send any message, so I went with 200)
+response.status(200).send("Author deleted")
+})
 
 export default authorsRouter
-
-/*
-
-// 1.
-usersRouter.post("/", (request, response) => {
-  // 1. Read request body obtaining new user's data
-  console.log("BODY: ", request.body) // DO NOT FORGET 
-
-  // 2. Add some server generated informations (unique id, creation Date, ...)
-  const newUser = { ...request.body, createdAt: new Date(), id: uniqid() } // uniqid is a 3rd party module that generates unique identifiers
-  console.log(newUser)
-
-  // 3. Read users.json --> obtaining an array
-  const usersArray = JSON.parse(fs.readFileSync(usersJSONPath))
-
-  // 4. Add new user to the array
-  usersArray.push(newUser)
-
-  // 5. Write the array back to users.json file
-  fs.writeFileSync(usersJSONPath, JSON.stringify(usersArray)) // we cannot pass an array to this function, but we can pass the stringified version of it
-
-  // 6. Send a proper response back
-
-  response.status(201).send({ id: newUser.id })
-})
-
-// 2.
-usersRouter.get("/", (request, response) => {
-  // 1. Read the content of users.json file
-  const fileContent = fs.readFileSync(usersJSONPath) // You obtain a BUFFER object, which is MACHINE READABLE ONLY
-  console.log("FILE CONTENT: ", JSON.parse(fileContent))
-
-  // 2. Get back an array from the file
-  const usersArray = JSON.parse(fileContent) // JSON.parse() converts BUFFER into a real ARRAY
-
-  // 3. Send back the array as a response
-
-  response.send(usersArray)
-})
-
-// 3.
-usersRouter.get("/:userId", (request, response) => {
-  console.log("REQ PARAMS: ", request.params.userId)
-
-  // 1. Read the file --> obtaining an array
-  const usersArray = JSON.parse(fs.readFileSync(usersJSONPath))
-
-  // 2. Find the specific user by id in the array
-  const foundUser = usersArray.find(user => user.id === request.params.userId)
-
-  // 3. Send a proper response
-  response.send(foundUser)
-})
-
-// 4.
-usersRouter.put("/:userId", (request, response) => {
-  // 1. Read the content of the file --> obtaining an array of users
-  const usersArray = JSON.parse(fs.readFileSync(usersJSONPath))
-
-  // 2. Modify specified user into the array by merging previous properties and new properties coming from req.body
-  const index = usersArray.findIndex(user => user.id === request.params.userId)
-  const oldUser = usersArray[index]
-  const updatedUser = { ...oldUser, ...request.body, updatedAt: new Date() }
-
-  usersArray[index] = updatedUser
-
-  // 3. Save file back with the updated list of users
-  fs.writeFileSync(usersJSONPath, JSON.stringify(usersArray))
-
-  // 4. Send back a proper response
-
-  response.send(updatedUser)
-})
-
-// 5.
-usersRouter.delete("/:userId", (request, response) => {
-  // 1. Read the file --> obtaining an array of users
-  const usersArray = JSON.parse(fs.readFileSync(usersJSONPath))
-
-  // 2. Filter out the specified user from the array, obtaining an array of just the remaining users
-  const remainingUsers = usersArray.filter(user => user.id !== request.params.userId) // ! = =
-
-  // 3. Save the remaining users back to users.json file
-  fs.writeFileSync(usersJSONPath, JSON.stringify(remainingUsers))
-
-  // 4. Send a proper response
-
-  response.status(204).send()
-
-
-})
-
-GET /authors => returns the list of authors
-GET (single user) /authors/123 => returns a single author
-POST /authors => create a new author
-PUT (single user) /authors/123 => edit the author with the given id
-DELETE (single user) /authors/123 => delete the author with the given id
-*/
